0d929ed83b5482a21426aa6ebdb415b304dc2f53
Mayant Mukul <mayantmukul@hotmail.com>
2021-01-09 15:56:06 +0530

    Add skill tree layouts

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-
-
-	$("#portfolio-contant-active").mixItUp();
-
-
 	$("#testimonial-slider").owlCarousel({
 	    paginationSpeed : 500,      
 	    singleItem:true,
@@ -26,28 +21,4 @@ $(document).ready(function(){
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
 	});
-
-
-	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
-
-
-	// Counter
-
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-
 });
-
-
-
-
